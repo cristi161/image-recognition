50ddbbcd30d03748d88f25f88b7b538597b48488
BogdanVoicu <50364988+bogdanvoicu72@users.noreply.github.com>
2020-04-28 19:33:43 +0300

    Cred ca trimit ce trebuie. Btw asta este verisiunea 1.0 care poate da upload la poze si le pune intr-un folder.

---
+++ site_web/urls.py
@@ -0,0 +1,7 @@
+from django.urls import  path
+from site_web import views
+
+urlpatterns = [
+    path('', views.pagina, name ='site_web'),
+
+]
---
+++ site_web/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+from django.shortcuts import redirect
+from .models import *
+from .forms import *
+from django.core.files.storage import FileSystemStorage
+from django.views.generic import TemplateView
+# Create your views here.
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+def pagina(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+    return  render(request, 'charecog.html', {})
+
+
